d42eab88df6e37fc0ecc63ae51009c3713491cef
Priyanka Mahajan <pm01.03.14@gmail.com>
2021-10-06 20:08:31 +0530

    Sound added for first

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
-for(var i = 0 ; i<document.querySelectorAll(".drum").length ;i++)
+/*for(var i = 0 ; i<document.querySelectorAll(".drum").length ;i++)
 {
   document.querySelectorAll(".drum")[i].addEventListener("click", function()
   {
     alert("Selected");
   });
-}
+}*/
+
+
+var audio1 = new Audio("sounds/crash.mp3");
+document.querySelectorAll(".drum")[0].addEventListener("click", function(){audio1.play();});

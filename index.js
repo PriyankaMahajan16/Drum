d6222d3c5ff2d94e870b3426de993b27915dad6c
Priyanka Mahajan <pm01.03.14@gmail.com>
2021-10-06 20:54:02 +0530

    Sounds added to specific instrument

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 
-/*for(var i = 0 ; i<document.querySelectorAll(".drum").length ;i++)
+for(var i =0; i< document.querySelectorAll(".drum").length; i ++)
 {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function()
-  {
-    alert("Selected");
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+
+        switch(this.innerHTML)
+        {
+          case "w":
+            new Audio("sounds/crash.mp3").play();
+            break;
+
+          case "a":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+
+          case "s":
+            new Audio("sounds/snare.mp3").play();
+            break;
+
+          case "d":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+
+          case "j":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+
+          case "k":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+
+          case "l":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+
+          default:
+            alert("Not valid");
+
+        }
+
+
+
+
+
+
+
   });
-}*/
-
-
-document.querySelectorAll(".drum")[0].addEventListener("click", function(){new Audio("sounds/crash.mp3").play();});
+}
